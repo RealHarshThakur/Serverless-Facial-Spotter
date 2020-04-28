bb5559dcec7e79e8f0cebe4b3844b76e0250f3b1
Mihir Shah <mihir.shah8260@gmail.com>
2020-04-29 02:55:02 +0530

    Update handler.py

--- backend/handler.py
+++ backend/handler.py
@@ -39,18 +39,6 @@ def lambda_handler(event, context):
         newName = user_input+str(res)
         return newName 
     
-    def deleteAll():
-        s3 = boto3.resource('s3')
-        bucket=s3.Bucket(buck_name)
-        bucket.objects.all().delete()
-        return 1
-    def countObject():
-        conn = S3Connection('access-key','secret-access-key')
-        bucket = conn.get_bucket('bucket')
-        for key in bucket.list():
-            if (key.name)[-3] == "png":
-                return False
-
     response = {
         "statusCode": 200,
         "body": json.dumps(response1)
