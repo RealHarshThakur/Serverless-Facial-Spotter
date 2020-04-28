2371b2fc5956b2749ba0b90070fbbfe12e758e97
Mihir Shah <mihir.shah8260@gmail.com>
2020-04-29 02:29:48 +0530

    Update handler.py
    added the functionality for not allowing users to upload more than 3 photos

--- backend/handler.py
+++ backend/handler.py
@@ -8,16 +8,17 @@ from PIL import Image
 import PIL.Image
 import random
 import string
+from boto.s3.connection import S3Connection
 
 s3_client = boto3.client('s3')
-
+buck_name=''
+response1=''
 def lambda_handler(event, context):
-    buck_name=''
     namee=bucket_name(buck_name)
-    
-    response = s3_client.create_bucket(
+    noImages=countObject()
+    response1 = s3_client.create_bucket(
     ACL='public',
-    Bucket={namee},
+    Bucket=namee,
     CreateBucketConfiguration={
         'LocationConstraint': 'ap-south-1'
     })
@@ -25,28 +26,25 @@ def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = unquote_plus(record['s3']['object']['key'])
-        upload_path=''
+        upload_path=''.join(namee)
         s3_client.upload_file(upload_path,bucket, key)
 
 
-def bucket_name(Throw_user_input):
-    N = 7
-    res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N)) 
-    newName = Throw_user_input+str(res)
-    return newName 
+    def bucket_name(user_input):
+        N = 7
+        res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N)) 
+        newName = user_input+str(res)
+        return newName 
+    
+    def countObject():
+        conn = S3Connection('access-key','secret-access-key')
+        bucket = conn.get_bucket('bucket')
+        for key in bucket.list():
+            if (key.name)[-3] == "png":
+                return False
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(res)
-    }
-
+        "body": json.dumps(response1)
+        }
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
