877120d8f66b9d04020fb99f9643077787eaa496
Mihir Shah <mihir.shah8260@gmail.com>
2020-04-29 02:44:00 +0530

    Updated backend & frontend integration
    Added the functionality to delete all the images

--- backend/handler.py
+++ backend/handler.py
@@ -11,11 +11,14 @@ import string
 from boto.s3.connection import S3Connection
 
 s3_client = boto3.client('s3')
+
 buck_name=''
 response1=''
 def lambda_handler(event, context):
     namee=bucket_name(buck_name)
     noImages=countObject()
+
+    delete=deleteAll()
     response1 = s3_client.create_bucket(
     ACL='public',
     Bucket=namee,
@@ -36,6 +39,11 @@ def lambda_handler(event, context):
         newName = user_input+str(res)
         return newName 
     
+    def deleteAll():
+        s3 = boto3.resource('s3')
+        bucket=s3.Bucket(buck_name)
+        bucket.objects.all().delete()
+        return 1
     def countObject():
         conn = S3Connection('access-key','secret-access-key')
         bucket = conn.get_bucket('bucket')
